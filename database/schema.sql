5d51f20f1f793fc38cf460aecc9c30c564abd909
Yuhan Zhang <zyh514312@gmail.com>
2022-07-26 22:57:28 +0000

    server and database setip

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,16 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."code-journal" (
+	"entryId" serial NOT NULL,
+	"html" TEXT,
+	"css" TEXT,
+	"javascript" TEXT,
+	"title" TEXT NOT NULL,
+	"imageUrl" TEXT NOT NULL,
+	"description" TEXT NOT NULL,
+	CONSTRAINT "code-journal_pk" PRIMARY KEY ("entryId")
+) WITH (
+  OIDS=FALSE
+);
--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 require('dotenv/config');
+const pg = require('pg');
 const path = require('path');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
+const db = new pg.Pool({ // eslint-disable-line
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
 const publicPath = path.join(__dirname, 'public');
@@ -11,9 +18,23 @@ if (process.env.NODE_ENV === 'development') {
 } else {
   app.use(express.static(publicPath));
 }
+app.use(express.json());
 
 app.get('/api/hello', (req, res) => {
   res.json({ hello: 'world' });
+});
+
+app.post('/api/code', (req, res, next) => {
+
+  const sql = `
+    insert into "code-journal"("html","css","javascript","title","imageUrl","description")
+    values ($1,$2,$3,'','','')
+    returning *
+  `;
+  const codeArray = [req.body.html, req.body.css, req.body.javascript];
+  db.query(sql, codeArray).then(result => {
+    res.status(200).json(result.rows[0]);
+  }).catch(err => next(err));
 });
 
 app.use(errorMiddleware);
