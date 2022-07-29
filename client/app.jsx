ce50095aa3133dc61f80a1e4df2241a68db08782
Yuhan Zhang <zyh514312@gmail.com>
2022-07-29 00:46:39 +0000

    page direct test success

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import Home from './pages/home';
+import ViewEntries from './pages/view-entries';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', event => {
+      this.setState({ route: parseRoute(window.location.hash) });
+    });
+  }
+
+  renderPage() {
+    const { route } = this.state;
+    if (route.path === 'hi') {
+      return <Home />;
+    }
+    if (route.path === 'entries') {
+      return <ViewEntries />;
+    }
     return <Home />;
+  }
+
+  render() {
+    return (<>{this.renderPage()}</>);
   }
 }
---
+++ client/pages/view-entries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ViewEntries extends React.Component {
+
+  render() {
+    return (
+      <><div>hello world</div></>
+    );
+  }
+
+}
