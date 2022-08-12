d822daace54aaf762d416502b7cd26c2a30aaae5
Yuhan Zhang <zyh514312@gmail.com>
2022-08-12 00:36:17 +0000

    invalid login handled demo user added

--- client/pages/Auth.jsx
+++ client/pages/Auth.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Auth(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('Testing-Account');
+  const [password, setPassword] = useState('Testing-Account');
   const [dataView, setDataView] = useState(props.dataView);
+  const [invalid, setinvalid] = useState(false);
 
   useEffect(() => {
     setDataView(props.dataView);
@@ -24,10 +25,14 @@ export default function Auth(props) {
       fetch('/api/auth/sign-in', { method: 'POST', body: JSON.stringify(newObject), headers: { 'Content-Type': 'application/json' } })
         .then(res => res.json())
         .then(result => {
+          if (result.error) {
+            setinvalid(true);
+          }
           if (result.token && result.user) {
             window.location.hash = '#entries';
             window.localStorage.setItem('react-context-jwt', result.token);
             window.localStorage.setItem('userId', result.user.userId);
+            setinvalid(false);
           }
         });
     }
@@ -70,18 +75,25 @@ export default function Auth(props) {
           />
         </div>
         <div>
-          {dataView === 'sign-up' &&
+            {(dataView === 'sign-up' && invalid === false) &&
           <div className='auth-button-div'>
             <a href="#sign-in" className='auth-link'>Already have account?</a>
             <button className='auth-button'>Sign Up</button>
           </div>
           }
-          {dataView === 'sign-in' &&
+          {(dataView === 'sign-in' && invalid === false) &&
               <div className='auth-button-div'>
                 <a href="#sign-up" className='auth-link'>Need an Account? </a>
                 <button className='auth-button'>Sign In</button>
               </div>
           }
+            {invalid === true &&
+              <div className='auth-button-div'>
+                <a className='auth-link'>Invalid Login</a>
+                <a href="#sign-up" className='auth-invalid-link' onClick={() => { setinvalid(false); }}>Sign Up? </a>
+                <button className='auth-button'>Sign In</button>
+              </div>
+            }
         </div>
       </form>
     </div>
